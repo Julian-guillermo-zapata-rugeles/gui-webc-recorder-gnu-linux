72f5e5070feb843732f596ec438b22760a2e2533
static <latanic@colossus.static>
2021-02-16 09:34:53 -0500

    se añadió las opciones de lanzamiento (varias)

--- ventanaprincipal.cpp
+++ ventanaprincipal.cpp
@@ -7,6 +7,10 @@ ventanaPrincipal::ventanaPrincipal(QWidget *parent) :
 {
     ui->setupUi(this);
     crearCarpetaGrabacion();
+    this->disparos=0;
+    this->timer = new QTimer();
+    connect(timer,SIGNAL(timeout()),this,SLOT(ejecutar()));
+    ui->informacion->setText("Bienvenido");
 }
 
 ventanaPrincipal::~ventanaPrincipal()
@@ -24,7 +28,28 @@ void ventanaPrincipal::on_salir_clicked()
 
 void ventanaPrincipal::crearCarpetaGrabacion()
 {
-   system("mkdir $HOME/webc");
+    system("mkdir $HOME/webc");
+}
+
+
+
+
+void ventanaPrincipal::ejecutar()
+{
+    QDateTime dateTime = dateTime.currentDateTime();
+    QString dateTimeString = dateTime.toString("yyyy-MM-dd hh:mm:ss");
+    dateTimeString.replace(" ","");
+    std::string time_now = "ffmpeg  -f video4linux2 -i /dev/video0 -f alsa -i hw:0 -acodec mp2 -qscale 0 -t 55 $HOME/webc/"+dateTimeString.toStdString()+".avi ";
+    qDebug() << QString(time_now.c_str());
+    system(time_now.c_str());
+    this->disparos -= 1;
+    if(disparos==0){
+        timer->stop();
+         ui->informacion->setText("Finalizado "+QString::number(this->disparos));
+    }
+    else {
+        ui->informacion->setText("Restantes "+QString::number(this->disparos));
+    }
 }
 
 
@@ -33,16 +58,44 @@ void ventanaPrincipal::crearCarpetaGrabacion()
 
 void ventanaPrincipal::on_shoot1_clicked()
 {
-    //system("notify-send 'Single Shoot'");
-    QDateTime dateTime = dateTime.currentDateTime();
-    QString dateTimeString = dateTime.toString("yyyy-MM-dd hh:mm:ss");
-    std::string time_now = "ffmpeg  -f video4linux2 -i /dev/video0 -f alsa -i hw:0 -acodec mp2 -qscale 0 -t 55 $HOME/webc/"+dateTimeString.toStdString()+".avi ";
-    qDebug() << QString(time_now.c_str());
-    system(time_now.c_str());
+    system("notify-send 'Single Shoot'");
+    this->disparos=1;
+    ejecutar();
+    ui->informacion->setText("Ejecutandose");
+
 }
+
 
 void ventanaPrincipal::on_dependencias_clicked()
 {
     system("notify-send 'copie y pegue este comando en su terminal'");
+    ui->informacion->setText("sudo apt-get install ffmpeg");
+    ui->informacion->setStyleSheet("p align:center;");
+}
 
+void ventanaPrincipal::on_shoot10_clicked()
+{
+    system("notify-send '5 shoots'");
+    this->disparos=5;
+    ejecutar();
+    this->timer->start(60000);
+    ui->informacion->setText("Ejecutandose");
+}
+
+void ventanaPrincipal::on_shoot50_clicked()
+{
+    system("notify-send '10 shoots'");
+    this->disparos=10;
+    ejecutar();
+    this->timer->start(60000);
+    ui->informacion->setText("Ejecutandose");
+}
+
+void ventanaPrincipal::on_shoot100_clicked()
+{
+    system("notify-send '50 shoots'");
+    this->disparos=50;
+    ejecutar();
+    this->timer->start(60000);
+    ui->informacion->setText("Ejecutandose");
 }
--- ventanaprincipal.h
+++ ventanaprincipal.h
@@ -21,14 +21,25 @@ public:
 
 private slots:
     void on_salir_clicked();
-
     void on_shoot1_clicked();
-
     void on_dependencias_clicked();
+    void ejecutar();
+
+    void on_shoot10_clicked();
+
+    void on_shoot50_clicked();
+
+    void on_shoot100_clicked();
 
 private:
+
+    unsigned short int disparos;
+    QTimer *timer;
+
+
     Ui::ventanaPrincipal *ui;
     void crearCarpetaGrabacion();
+
 };
 
 #endif // VENTANAPRINCIPAL_H
